b054fd2e98e70fdcd65d251976813eff88e118a0
j1seon9 <j1s3on9@gmail.com>
2025-08-20 09:58:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -267,7 +267,7 @@ app.get("*", (req, res) => {
 });
 
 // --------- start ---------
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-
